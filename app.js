c1377cc016716fb9c8c8a3ecce2a26fe24d38d05
vishalchauhan91196 <vishalchauhan91196@users.noreply.github.com>
2021-03-31 17:43:08 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const txtInput = document.querySelector("#txt-input");
+const btnTranslate = document.querySelector(".btn-translate");
+const txtOutput = document.querySelector(".output");
+
+const getURL = function (text) {
+  return "https://api.funtranslations.com/translate/dothraki.json?text=" + text;
+};
+
+const errorHandler = function (error) {
+  alert("Something went wrong.");
+  console.log(error.message);
+};
+
+const translator = function () {
+  fetch(getURL(txtInput.value))
+    .then((response) => response.json())
+    .then((data) => (txtOutput.innerText = data.contents.translated))
+    .catch(errorHandler);
+};
+
+btnTranslate.addEventListener("click", translator);
